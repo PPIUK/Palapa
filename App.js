0cfadce55cdc33df5b4faee88f8311c8b1de46c2
evanrussel15 <66687431+evanrussel15@users.noreply.github.com>
2022-02-18 20:00:51 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Palapa!!</Text>
+      <Image source={require("./assets/logoPPIUK.png")} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
