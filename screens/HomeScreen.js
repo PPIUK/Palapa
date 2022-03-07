32c18cc1c4a38ddb490f152901ebb8def86f7345
Lordifal <72856910+Lordifal@users.noreply.github.com>
2022-03-07 21:43:11 +0000

    Update HomeScreen.js
    Rapihkan button dan logo PPI UK + navigations

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,37 +1,94 @@
 import * as React from 'react';
-import { Button, Text, View, StyleSheet, Image, Dimensions } from 'react-native';
+import { Button, Text, View, Image, StyleSheet, Dimensions } from 'react-native';
 import Constants from 'expo-constants';
 
 export default function HomeScreen({ navigation }) {
   return (
     <>
-    <View style={{ alignItems: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+    <View>
+      <Text style={styles.header}>PPI UK APP</Text>
+      <Image style={styles.image} source={require('../assets/PPIUK.png')} />
+      </View >
+      <View style={styles.container}>
+      <Button 
+        title="Transportasi"
+        fontSize="20"
+        margin="10"
+        onPress={() => navigation.navigate('Transportasi')}
       />
-      <Text style={styles.paragraph}>
-        PPI UK APP PALAPA
-      </Text>
-      <Image style={{...styles.logo, justifyContent:'center', width: Dimensions.get('window').width - 50, resizeMode:'contain'}} source={require('../assets/PPIUK.png')} />
-    </View>
+      <Text style={styles.button1}> </Text>
+      <Button 
+        title="Checklist keberangkatan UK"
+        fontSize="10"
+        color="#AA336A"      
+        onPress={() => navigation.navigate('Keberangkatan')}
+      />
+      <Text style={styles.button2}> </Text>
+      <Button 
+        title="Checklist Kepulangan ke ID"
+        fontSize="20"
+        color="#AA336A"      
+        onPress={() => navigation.navigate('Kepulangan')}
+      />
+      <Text style={styles.button1}> </Text>
+      <Button 
+        title="Info terkait Imigrasi"
+        fontSize="20"
+        color="#AA336A"      
+        onPress={() => navigation.navigate('Imigrasi')}
+      />
+      <Text style={styles.button1}> </Text>
+      <Button 
+        title="Kebijakan Terbaru dari Pemerintah UK & Indonesia"
+        fontSize="20"
+        color="#013220"      
+        onPress={() => navigation.navigate('News')}
+      />
+      <Text style={styles.button1}> </Text>
+      <Button 
+        title="Daftar Restoran & Catering Indonesia di UK"
+        fontSize="20"
+        color="#9b870c"      
+        onPress={() => navigation.navigate('Restoran Indonesia')}
+      />                  
+  </View>     
       </>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'flex-start',
-      paddingTop: Constants.statusBarHeight,
-      backgroundColor: '#ecf0f1',
-      padding: 8,
-    },
-    paragraph: {
-      margin: 24,
-      fontSize: 18,
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ffffff',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  header: {
+    margin: 24,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }, 
+ image: {
+    alignSelf: 'center',
+    width: Dimensions.get('window').width - 50,
+    resizeMode:'contain',
+  }, 
+  button1: {
+    margin: 1,
+    fontSize: 10,
+    color:'black',    
+  },    
+  button2: {
+    margin: 1,
+    fontSize: 10,
+    color:'#FFFFFF',    
+  },    
+});
