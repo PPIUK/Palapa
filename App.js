87f44a77ae44f9ca236ce43a85a589285d026643
Frenciel Anggi <frencielanggi02@gmail.com>
2022-06-09 19:21:28 +0100

    splash screen animation

--- App.js
+++ App.js
@@ -1,12 +1,114 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { Asset } from "expo-asset";
+import Constants from "expo-constants";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Animated, StyleSheet, View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { MainDrawer } from './nav/Drawer'
 
+// Main app function
 export default function App() {
- return (
-    <NavigationContainer>
-      <MainDrawer/>
-    </NavigationContainer>
- );
+  return (
+    // Calling animation on splash screen
+    <AnimatedAppLoader image={{ uri: Constants.manifest.splash.image }}>
+
+      {/* Main content */}
+      <NavigationContainer>
+        <MainDrawer/>
+      </NavigationContainer>
+      {/*  */}
+
+    </AnimatedAppLoader>
+  );
+}
+
+// Splash Screen
+// Instruct SplashScreen not to hide yet, we want to do this manually
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
+
+// Animation function
+function AnimatedAppLoader({ children, image }) {
+  const [isSplashReady, setSplashReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      await Asset.fromURI(image.uri).downloadAsync();
+      setSplashReady(true);
+    }
+
+    prepare();
+  }, [image]);
+
+  if (!isSplashReady) {
+    return null;
+  }
+
+  return <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>;
+}
+
+// Splash screen animation function
+function AnimatedSplashScreen({ children, image }) {
+  const animation = useMemo(() => new Animated.Value(1), []);
+  const [isAppReady, setAppReady] = useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isAppReady]);
+
+  const onImageLoaded = useCallback(async () => {
+    try {
+      await SplashScreen.hideAsync();
+      // Load stuff
+      await Promise.all([]);
+    } catch (e) {
+      // handle errors
+    } finally {
+      setAppReady(true);
+    }
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isAppReady && children}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: Constants.manifest.splash.backgroundColor,
+              opacity: animation,
+            },
+          ]}
+        >
+          <Animated.Image
+            style={{
+              width: "100%",
+              height: "100%",
+              resizeMode: Constants.manifest.splash.resizeMode || "contain",
+              transform: [
+                {
+                  scale: animation,
+                },
+              ],
+            }}
+            source={image}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
 }
